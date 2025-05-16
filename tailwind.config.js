1877188c855434a5e3b5f5d57d36d15c27f094d2
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-05-16 15:11:31 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'burn-bg': '#0A0A0A',
+        'burn-neon': '#39FF14',
+      },
+      boxShadow: {
+        neon: '0 0 8px rgba(57,255,20,0.7)',
+      },
+      textShadow: {
+        neon: '0 0 4px rgba(57,255,20,0.9)',
+      },
+    }
+  },
+  plugins: [
+    require('tailwindcss-textshadow')
+  ]
+}
