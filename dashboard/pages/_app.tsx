e844a43074f8815cfa587db1a19e85c696fa7050
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-05-16 15:24:36 -0400

    Update _app.tsx

--- dashboard/pages/_app.tsx
+++ dashboard/pages/_app.tsx
@@ -1,5 +1,5 @@
 // dashboard/pages/_app.tsx
-import '../styles/globals.css';
+import '../styles/global.css';
 export default function App({ Component, pageProps }) {
   return (
     <div className="min-h-screen bg-burn-bg text-burn-neon font-mono">
